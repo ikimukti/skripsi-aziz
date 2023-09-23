793b98de6c1400bd4ecf7a21df25dced18742695
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2023-09-23 11:09:24 +0700

    asdfsafsa

---
+++ public/exams/examsCreate.php
@@ -0,0 +1,238 @@
+<?php
+// Initialize the session
+session_start();
+
+require_once('../../database/connection.php');
+
+// Initialize errors array
+$errors = array();
+
+// Process form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Validate and sanitize input data
+  $subject_id = $_POST['subject_id'];
+  $title = $_POST['title'];
+  $type = $_POST['type'];
+  $sequence = $_POST['sequence'];
+
+  // Validate Sequence
+  if (empty($sequence)) {
+    $errors['sequence'] = "Sequence is required.";
+  } elseif (!is_numeric($sequence)) {
+    $errors['sequence'] = "Sequence must be a number.";
+  } else {
+    // Check if the sequence already exists in the database
+    $queryCheckSequence = "SELECT id FROM post_tests WHERE sequence = ?";
+    $stmtCheckSequence = $conn->prepare($queryCheckSequence);
+    $stmtCheckSequence->bind_param('i', $sequence);
+    $stmtCheckSequence->execute();
+    $resultCheckSequence = $stmtCheckSequence->get_result();
+
+    if ($resultCheckSequence->num_rows > 0) {
+      $errors['sequence'] = "Sequence already exists.";
+    }
+
+    $stmtCheckSequence->close();
+  }
+
+  // Check for errors
+  if (empty($subject_id)) {
+    $errors['subject_id'] = "Please select a subject.";
+  }
+
+  if (empty($title)) {
+    $errors['title'] = "Title is required.";
+  }
+
+  if (empty($type)) {
+    $errors['type'] = "Type is required.";
+  }
+
+  // Validate and process questions
+  $questions = array();
+  if (isset($_POST['questions']) && is_array($_POST['questions'])) {
+    foreach ($_POST['questions'] as $questionData) {
+      $question = $questionData['question'];
+      $options = $questionData['options'];
+      $answer = $questionData['answer'];
+      $explanation = $questionData['explanation'];
+
+      // Validate question data here if needed
+
+      // Add question to the questions array
+      $questions[] = array(
+        'question' => $question,
+        'options' => $options,
+        'answer' => $answer,
+        'explanation' => $explanation
+      );
+    }
+  } else {
+    $errors['questions'] = "At least one question is required.";
+  }
+
+  // If no errors, insert data into the database
+  if (empty($errors)) {
+    // Insert data into the post_tests table
+    $questionsJSON = json_encode($questions);
+    $sql = "INSERT INTO post_tests (subject_id, title, type, questions, sequence) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('isssi', $subject_id, $title, $type, $questionsJSON, $sequence);
+
+    if ($stmt->execute()) {
+      // Redirect to examsList page or any other destination
+      header('location: examsList.php');
+    } else {
+      // Insertion failed
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+  }
+}
+
+// Fetch subjects from the database
+$querySubjects = "SELECT id, subject_name FROM subjects";
+$resultSubjects = $conn->query($querySubjects);
+
+// Close the database connection
+$conn->close();
+?>
+
+<?php include_once('../components/header.php'); ?>
+<!-- Main Content Height Menyesuaikan Hasil Kurang dari Header dan Footer -->
+<div class="h-screen flex flex-col">
+  <!-- Top Navbar -->
+  <?php include('../components/navbar.php'); ?>
+  <!-- End Top Navbar -->
+  <!-- Main Content -->
+  <div class="flex-grow bg-gray-50 flex flex-row shadow-md">
+    <!-- Sidebar -->
+    <?php include('../components/sidebar.php'); ?>
+    <!-- End Sidebar -->
+
+    <!-- Main Content -->
+    <main class="bg-gray-50 text-white flex-1 overflow-y-scroll h-screen w-screen sc-hide">
+      <div class="flex items-start justify-start p-6 shadow-md m-4 flex-1 flex-col">
+        <!-- Header Content -->
+        <div class="flex flex-row justify-between items-center w-full border-b-2 border-gray-600 mb-2 pb-2">
+          <h1 class="text-3xl text-gray-800 font-semibold w-full">Create Exam</h1>
+          <a href="../exams/examsList.php" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center space-x-2">
+            <i class="fas fa-arrow-left"></i>
+            <span>Back</span>
+          </a>
+        </div>
+        <!-- End Header Content -->
+        <!-- Content -->
+        <div class="flex flex-col w-full">
+          <!-- Navigation -->
+          <div class="flex flex-row justify-between items-center w-full pb-2">
+            <div>
+              <h2 class="text-lg text-gray-800 font-semibold">Welcome back, <?php echo $_SESSION['fullname']; ?>!</h2>
+              <p class="text-gray-600 text-sm">Exam information.</p>
+            </div>
+          </div>
+          <!-- End Navigation -->
+          <!-- Form Create -->
+          <form action="" method="POST" class="flex flex-col w-full space-x-2" enctype="multipart/form-data">
+            <!-- Subject Dropdown -->
+            <label for="subject_id" class="block font-semibold text-gray-800 mt-2 mb-2">Select Subject <span class="text-red-500">*</span></label>
+            <select id="subject_id" name="subject_id" class="w-full rounded-md border-gray-300  px-2 py-2 border text-gray-600">
+              <option value="" disabled>Select a subject</option>
+              <?php
+              while ($rowSubject = $resultSubjects->fetch_assoc()) {
+                $subjectId = $rowSubject['id'];
+                $subjectName = $rowSubject['subject_name'];
+                $selected = ($_POST['subject_id'] == $subjectId) ? 'selected' : ''; // Check if the option should be selected
+                echo "<option value='$subjectId' $selected>$subjectName</option>";
+              }
+              ?>
+            </select>
+            <!-- Error Subject -->
+            <?php if (isset($errors['subject_id'])) : ?>
+              <p class="text-red-500 text-sm">
+                <?php echo $errors['subject_id']; ?>
+              </p>
+            <?php endif; ?>
+
+            <!-- Title -->
+            <label for="title" class="block font-semibold text-gray-800 mt-2 mb-2">Title <span class="text-red-500">*</span></label>
+            <input type="text" id="title" name="title" class="w-full rounded-md border-gray-300  px-2 py-2 border text-gray-600" placeholder="Title" value="<?php echo isset($_POST['title']) ? $_POST['title'] : ''; ?>">
+            <!-- Error Title -->
+            <?php if (isset($errors['title'])) : ?>
+              <p class="text-red-500 text-sm">
+                <?php echo $errors['title']; ?>
+              </p>
+            <?php endif; ?>
+
+            <!-- Type -->
+            <label for="type" class="block font-semibold text-gray-800 mt-2 mb-2">Type <span class="text-red-500">*</span></label>
+            <input type="text" id="type" name="type" class="w-full rounded-md border-gray-300  px-2 py-2 border text-gray-600" placeholder="Type" value="<?php echo isset($_POST['type']) ? $_POST['type'] : ''; ?>">
+            <!-- Error Type -->
+            <?php if (isset($errors['type'])) : ?>
+              <p class="text-red-500 text-sm">
+                <?php echo $errors['type']; ?>
+              </p>
+            <?php endif; ?>
+
+            <!-- Sequence -->
+            <label for="sequence" class="block font-semibold text-gray-800 mt-2 mb-2">Sequence <span class="text-red-500">*</span></label>
+            <input type="text" id="sequence" name="sequence" class="w-full rounded-md border-gray-300  px-2 py-2 border text-gray-600" placeholder="Sequence" value="<?php echo isset($_POST['sequence']) ? $_POST['sequence'] : ''; ?>">
+            <!-- Error Sequence -->
+            <?php if (isset($errors['sequence'])) : ?>
+              <p class="text-red-500 text-sm">
+                <?php echo $errors['sequence']; ?>
+              </p>
+            <?php endif; ?>
+
+            <!-- Questions Section -->
+            <label class="block font-semibold text-gray-800 mt-2 mb-2">Questions <span class="text-red-500">*</span></label>
+            <div id="questions-container">
+              <!-- Initial question fields -->
+              <div class="question">
+                <label for="question1">Question 1:</label>
+                <input type="text" name="questions[1][question]" id="question1" placeholder="Enter the question" required>
+                <label for="options1">Options:</label>
+                <input type="text" name="questions[1][options][]" placeholder="Option 1" required>
+                <input type="text" name="questions[1][options][]" placeholder="Option 2" required>
+                <input type="text" name="questions[1][options][]" placeholder="Option 3" required>
+                <input type="text" name="questions[1][options][]" placeholder="Option 4" required>
+                <label for="answer1">Correct Answer:</label>
+                <select name="questions[1][answer]" id="answer1">
+                  <option value="1">Option 1</option>
+                  <option value="2">Option 2</option>
+                  <option value="3">Option 3</option>
+                  <option value="4">Option 4</option>
+                </select>
+              </div>
+            </div>
+            <button type="button" id="add-question">Add Question</button>
+            <!-- Error Questions -->
+            <?php if (isset($errors['questions'])) : ?>
+              <p class="text-red-500 text-sm">
+                <?php echo $errors['questions']; ?>
+              </p>
+            <?php endif; ?>
+
+            <!-- Create Button -->
+            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4">
+              <i class="fas fa-plus mr-2"></i>
+              <span>Create</span>
+            </button>
+          </form>
+          <!-- End Form Create -->
+        </div>
+        <!-- End Content -->
+      </div>
+      <hr class=" w-full h-40 mt-40">
+    </main>
+    <!-- End Main Content -->
+  </div>
+  <!-- End Main Content -->
+</div>
+<!-- Footer -->
+<?php include('../components/footer.php'); ?>
+<!-- End Footer -->
+</div>
+<!-- End Main Content -->
+</body>
+
+</html>
---
+++ public/exams/examsList.php
@@ -0,0 +1,294 @@
+<?php
+// Initialize the session
+session_start();
+// Include the connection file
+require_once('../../database/connection.php');
+
+// Initialize variables
+$username = $password = '';
+$errors = array();
+$selectedSubject = ''; // Variable to store the selected subject
+$selectedTestType = ''; // Variable to store the selected test type
+
+// Check if a subject is selected from the dropdown
+if (isset($_GET['subject']) && $_GET['subject'] !== '') {
+  $selectedSubject = $_GET['subject'];
+}
+
+// Check if a test type is selected from the dropdown
+if (isset($_GET['test_type']) && $_GET['test_type'] !== '') {
+  $selectedTestType = $_GET['test_type'];
+}
+
+// Close the database connection
+?>
+<?php include_once('../components/header.php'); ?>
+<!-- Main Content Height Menyesuaikan Hasil Kurang dari Header dan Footer -->
+<div class="h-screen flex flex-col">
+  <!-- Top Navbar -->
+  <?php include('../components/navbar.php'); ?>
+  <!-- End Top Navbar -->
+  <!-- Main Content -->
+  <div class="flex-grow bg-gray-50 flex flex-row shadow-md">
+    <!-- Sidebar -->
+    <?php include('../components/sidebar.php'); ?>
+    <!-- End Sidebar -->
+
+    <!-- Main Content -->
+    <main class=" bg-gray-50 flex flex-col flex-1 overflow-y-scroll h-screen flex-shrink-0 sc-hide">
+      <div class="flex items-start justify-start p-6 shadow-md m-4 flex-1 flex-col">
+        <!-- Header Content -->
+        <div class="flex flex-row justify-between items-center w-full border-b-2 border-gray-600 mb-2 pb-2">
+          <h1 class="text-3xl text-gray-800 font-semibol w-full">Exams List</h1>
+          <div class="flex flex-row justify-end items-center">
+            <a href="../exams/examsCreate.php" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+              <i class="fas fa-plus mr-2"></i>
+              <span>Create</span>
+            </a>
+          </div>
+        </div>
+        <!-- End Header Content -->
+        <!-- Content -->
+        <div class="flex flex-col w-full">
+          <!-- Dropdown for Selecting Subjects -->
+          <div class="mb-4">
+            <label for="subjectDropdown" class="block text-gray-800 font-semibold">Select Subject:</label>
+            <select id="subjectDropdown" name="subjectDropdown" class="bg-white border border-gray-300 rounded-lg py-2 px-4 block w-full">
+              <option value="" <?php if ($selectedSubject === '') echo 'selected'; ?>>All Subjects</option>
+              <?php
+              // Fetch subjects from the database
+              $querySubjects = "SELECT id, subject_name FROM subjects ORDER BY subject_name ASC";
+              $resultSubjects = $conn->query($querySubjects);
+
+              while ($rowSubject = $resultSubjects->fetch_assoc()) {
+                $subjectId = $rowSubject['id'];
+                $subjectName = $rowSubject['subject_name'];
+                $selected = ($subjectId == $selectedSubject) ? 'selected' : '';
+
+                echo "<option value='$subjectId' $selected>$subjectName</option>";
+              }
+              ?>
+            </select>
+          </div>
+          <!-- End Dropdown -->
+
+          <!-- Dropdown for Selecting Test Type -->
+          <div class="mb-4">
+            <label for="testTypeDropdown" class="block text-gray-800 font-semibold">Select Test Type:</label>
+            <select id="testTypeDropdown" name="testTypeDropdown" class="bg-white border border-gray-300 rounded-lg py-2 px-4 block w-full">
+              <option value="all" <?php if ($selectedTestType === 'all') echo 'selected'; ?>>All Test Types</option>
+              <option value="post_test" <?php if ($selectedTestType === 'post_test') echo 'selected'; ?>>Post Test</option>
+              <option value="pre_test" <?php if ($selectedTestType === 'pre_test') echo 'selected'; ?>>Pre Test</option>
+            </select>
+          </div>
+
+          <button onclick="applyFilter()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+            Apply Filter
+          </button>
+
+          <!-- End Dropdown -->
+
+          <!-- Navigation -->
+          <div class="flex flex-row justify-between items-center w-full mb-2 pb-2">
+            <div>
+              <h2 class="text-lg text-gray-800 font-semibold">Welcome back, <?php echo $_SESSION['fullname']; ?>!</h2>
+              <p class="text-gray-600 text-sm">Here are the list of exams.</p>
+            </div>
+            <!-- Search -->
+            <form class="flex items-center justify-end space-x-2 w-96">
+              <input type="text" name="search" class="bg-gray-200 focus:bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" placeholder="Search" value="<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>">
+              <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded space-x-2 inline-flex items-center">
+                <i class="fas fa-search"></i>
+                <span>Search</span>
+              </button>
+            </form>
+            <!-- End Search -->
+          </div>
+          <!-- End Navigation -->
+          <!-- Table -->
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <th class="text-left py-2">No</th>
+                <th class="text-left py-2">Title</th>
+                <th class="text-left py-2">Type</th>
+                <th class="text-left py-2">Questions</th>
+                <th class="text-left py-2">Subject</th>
+                <th class="text-left py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              // Build the SQL query based on selected subject and test type (if any)
+              $query = "SELECT e.id, e.title, e.type, e.questions, e.sequence, s.subject_name 
+                        FROM post_tests e
+                        LEFT JOIN subjects s ON e.subject_id = s.id
+                        WHERE 1 ";
+
+              if ($selectedSubject !== '') {
+                $query .= "AND s.id = $selectedSubject ";
+              }
+
+              if ($selectedTestType === 'post_test') {
+                $query .= "AND e.type = 'jawaban_singkat' ";
+              } elseif ($selectedTestType === 'pre_test') {
+                $query .= "AND e.type = 'jawaban_singkat' ";
+              }
+
+              $query .= "ORDER BY e.id ASC";
+
+              // Fetch exam data from the database limit by 15 data per page
+              $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
+              $page = isset($_GET['page']) ? $_GET['page'] : 1;
+              $query .= " LIMIT " . ($page - 1) * 15 . ", 15";
+              $result = $conn->query($query);
+
+              // Count total rows in the table
+              $queryCount = "SELECT COUNT(*) AS count 
+                             FROM post_tests e
+                             LEFT JOIN subjects s ON e.subject_id = s.id
+                             WHERE 1 ";
+
+              if ($selectedSubject !== '') {
+                $queryCount .= "AND s.id = $selectedSubject ";
+              }
+
+              if ($selectedTestType === 'post_test') {
+                $queryCount .= "AND e.type = 'jawaban_singkat' ";
+              } elseif ($selectedTestType === 'pre_test') {
+                $queryCount .= "AND e.type = 'jawaban_singkat' ";
+              }
+
+              $queryCount .= "AND (e.title LIKE '%" . $searchTerm . "%' OR
+                                   e.type LIKE '%" . $searchTerm . "%' OR
+                                   e.questions LIKE '%" . $searchTerm . "%')";
+
+              $resultCount = $conn->query($queryCount);
+              $rowCount = $resultCount->fetch_assoc()['count'];
+              $totalPage = ceil($rowCount / 15);
+
+              $no = 1;
+
+              // Loop through the results and display data in rows
+              while ($row = $result->fetch_assoc()) {
+              ?>
+                <tr>
+                  <td class="py-2"><?php echo $no++; ?></td>
+                  <td class="py-2"><?php echo $row['title']; ?></td>
+                  <td class="py-2"><?php echo $row['type']; ?></td>
+                  <td class="py-2"><?php echo $row['questions']; ?></td>
+                  <td class="py-2"><?php echo $row['subject_name']; ?></td>
+                  <td class='py-2'>
+                    <!-- Detail Button -->
+                    <a href="../exams/examsDetail.php?id=<?php echo $row['id'] ?>" class='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex items-center mr-2 text-sm'>
+                      <i class='fas fa-eye mr-2'></i>
+                      <span>Detail</span>
+                    </a>
+                    <!-- Edit Button -->
+                    <a href="../exams/examsUpdate.php?id=<?php echo $row['id'] ?>" class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center mr-2 text-sm'>
+                      <i class='fas fa-edit mr-2'></i>
+                      <span>Edit</span>
+                    </a>
+                    <!-- Delete Button -->
+                    <a href="../exams/examsDelete.php?id=<?php echo $row['id'] ?>" class='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-flex items-center text-sm'>
+                      <i class='fas fa-trash mr-2'></i>
+                      <span>Delete</span>
+                    </a>
+                  </td>
+                </tr>
+              <?php
+              }
+              if ($result->num_rows === 0) {
+              ?>
+                <tr>
+                  <td colspan="6" class="py-2 text-center">No data found.</td>
+                </tr>
+              <?php
+              }
+              ?>
+            </tbody>
+          </table>
+          <!-- End Table -->
+          <?php
+          // Pagination
+          ?>
+          <div class="flex flex-row justify-between items-center w-full mt-4">
+            <div class="flex flex-row justify-start items-center">
+              <span class="text-gray-600">Total <?php echo $rowCount; ?> rows</span>
+            </div>
+            <div class="flex flex-row justify-end items-center space-x-2">
+              <a href="?page=1&search=<?php echo $searchTerm; ?>&subject=<?php echo $selectedSubject; ?>&test_type=<?php echo $selectedTestType; ?>" class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded inline-flex items-center">
+                <i class="fas fa-angle-double-left"></i>
+              </a>
+              <a href="?page=<?php if ($page == 1) {
+                                echo $page;
+                              } else {
+                                echo $page - 1;
+                              } ?>&search=<?php echo $searchTerm; ?>&subject=<?php echo $selectedSubject; ?>&test_type=<?php echo $selectedTestType; ?>" class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded inline-flex items-center">
+                <i class="fas fa-angle-left"></i>
+              </a>
+              <!-- Page number -->
+              <?php
+              $startPage = $page - 2;
+              $endPage = $page + 2;
+              if ($startPage < 1) {
+                $endPage += abs($startPage) + 1;
+                $startPage = 1;
+              }
+              if ($endPage > $totalPage) {
+                $startPage -= $endPage - $totalPage;
+                $endPage = $totalPage;
+              }
+              if ($startPage < 1) {
+                $startPage = 1;
+              }
+              for ($i = $startPage; $i <= $endPage; $i++) {
+                if ($i == $page) {
+                  echo "<a href='?page=$i&search=$searchTerm&subject=$selectedSubject&test_type=$selectedTestType' class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center'>$i</a>";
+                } else {
+                  echo "<a href='?page=$i&search=$searchTerm&subject=$selectedSubject&test_type=$selectedTestType' class='bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded inline-flex items-center'>$i</a>";
+                }
+              }
+              ?>
+              <a href="?page=<?php if ($page == $totalPage) {
+                                echo $page;
+                              } else {
+                                echo $page + 1;
+                              } ?>&search=<?php echo $searchTerm; ?>&subject=<?php echo $selectedSubject; ?>&test_type=<?php echo $selectedTestType; ?>" class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded inline-flex items-center">
+                <i class="fas fa-angle-right"></i>
+              </a>
+              <a href="?page=<?php echo $totalPage; ?>&search=<?php echo $searchTerm; ?>&subject=<?php echo $selectedSubject; ?>&test_type=<?php echo $selectedTestType; ?>" class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-2 px-4 rounded inline-flex items-center">
+                <i class="fas fa-angle-double-right"></i>
+              </a>
+            </div>
+            <div class="flex flex-row justify-end items-center ml-2">
+              <span class="text-gray-600">Page <?php echo $page; ?> of <?php echo $totalPage; ?></span>
+            </div>
+          </div>
+        </div>
+        <!-- End Content -->
+      </div>
+      <hr class="mt-60">
+    </main>
+    <!-- End Main Content -->
+  </div>
+  <!-- End Main Content -->
+  <!-- Footer -->
+  <?php include('../components/footer.php'); ?>
+  <!-- End Footer -->
+</div>
+<!-- End Main Content -->
+
+<script>
+  // JavaScript function to apply the filter when a subject or test type is selected from the dropdowns
+  function applyFilter() {
+    const selectedSubject = document.getElementById('subjectDropdown').value;
+    const selectedTestType = document.getElementById('testTypeDropdown').value;
+    // Redirect to the same page with the selected subject and test type as query parameters
+    window.location.href = `examsList.php?subject=${selectedSubject}&test_type=${selectedTestType}`;
+  }
+</script>
+
+</body>
+
+</html>
