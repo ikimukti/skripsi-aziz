c6af4e5a56191049c00d6b9a3d8fae00dab6c5c7
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2023-10-10 15:04:10 +0700

    asfasfsaf

--- database/connection.php
+++ database/connection.php
@@ -1,11 +1,27 @@
 <?php
-$host = 'localhost'; // Ganti dengan host MySQL Anda
-$username = 'root'; // Ganti dengan username MySQL Anda
-$password = ''; // Ganti dengan password MySQL Anda
-$database = 'skripsi-aziz'; // Ganti dengan nama database Anda
+// $host = 'localhost'; // Ganti dengan host MySQL Anda
+// $username = 'root'; // Ganti dengan username MySQL Anda
+// $password = ''; // Ganti dengan password MySQL Anda
+// $database = 'skripsi-aziz'; // Ganti dengan nama database Anda
+
+// // Membuat koneksi
+// $conn = new mysqli($host, $username, $password, $database);
+
+// // Memeriksa koneksi
+// if ($conn->connect_error) {
+//     die("Koneksi gagal: " . $conn->connect_error);
+// }
+
+// // Base URL Configuration
+// $baseUrl = "http://localhost/skripsi-aziz/";
+
+$hostname = "localhost";
+$db_username = "ikiz5613_skripsi_aziz";
+$db_password = "yongalah";
+$db_name = "ikiz5613_skripsi-aziz";
 
 // Membuat koneksi
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($hostname, $db_username, $db_password, $db_name);
 
 // Memeriksa koneksi
 if ($conn->connect_error) {
@@ -13,4 +29,4 @@ if ($conn->connect_error) {
 }
 
 // Base URL Configuration
-$baseUrl = "http://localhost/skripsi-aziz/";
+$baseUrl = "https://asadulaziz.arbiet.my.id/";
